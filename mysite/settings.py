decb37b42fc0e0d6b4ffa9c1a6f925c73538a6c7
Nishanth <krishna.churchmal00@gmail.com>
2023-03-12 04:56:54 +0530

    changed static config files.telusko

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,4 +137,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/askme/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'askme/static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
