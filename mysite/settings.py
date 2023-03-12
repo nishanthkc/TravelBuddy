835738c26b66203d911383cd95b781192cd7c47d
Nishanth <krishna.churchmal00@gmail.com>
2023-03-12 20:06:24 +0530

    folders css and js added trial-5 staticfiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,4 +139,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
