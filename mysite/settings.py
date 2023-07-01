b3aa5224fd49a357290ac921377b2a2386407ffc
Nishanth <krishna.churchmal00@gmail.com>
2023-07-01 23:10:15 +0530

    added og-image in the headers.html. uncommented STATICFILES_DIRS in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -153,7 +153,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # trying to get the static images
 
 
 
